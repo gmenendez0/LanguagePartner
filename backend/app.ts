467aa1d55a24eafffd13ba2881bf338922505632
Bruno Quiroga <bquiroga@fi.uba.ar>
2024-05-15 18:40:28 -0300

    Agrego endpoint para agregar usuarios

--- backend/app.ts
+++ backend/app.ts
@@ -1,17 +1,38 @@
 import express, { Request, Response } from 'express';
+import User from './model/user';
+import { UserRepository } from './repo/userRepository';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+const userRepository = new UserRepository();
+
 app.use(express.json());
 
-// Routes
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+app.post('/users/new', (req: Request, res: Response) => {
+    const { city, name, email, password } = req.body;
+  
+    if (!city || !name || !email || !password) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+  
+    const newUser: User = {
+      id: uuidv4(),
+      city,
+      name,
+      email,
+      password,
+    };
+  
+    userRepository.addUser(newUser);
+    res.status(201).json(newUser);
 });
