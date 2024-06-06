0f68fb82e67cbc55740e87841abf8fa0c1a1400f
Guido Menendez <guidomenendez0@gmail.com>
2024-06-06 17:49:35 -0300

    Mejoras en la estructura de responses de Controller.ts

--- backend/controllers/Controller.ts
+++ backend/controllers/Controller.ts
@@ -10,41 +10,43 @@ const UNHANDLED_ERROR_OBJECT = { error: "Internal server error." };
 
 export abstract class Controller {
     //Pre: Must receive an object that can be converted to standard body media type.
-    //Pre: body must be in standard body media type format.
+    //Post: Sets the object as response body and sends the response with status code 200.
     protected okResponse<T>(res: Response, object: T): void {
         this.setUpAndSendResponse(res, object, HTTPResponseCode.OK);
     }
 
     //Pre: Must receive an object that can be converted to standard body media type.
-    //Pre: body must be in standard body media type format.
+    //Post: Sets the object as response body and sends the response with status code 201.
     protected createdResponse<T>(res: Response, object: T): void {
         this.setUpAndSendResponse(res, object, HTTPResponseCode.CREATED);
     }
 
     //Pre: Must receive an object that can be converted to standard body media type.
-    //Pre: body must be in standard body media type format.
+    //Post: Sets the object as response body and sends the response with status code 400.
     protected badRequestResponse<T>(res: Response, object: T): void {
         this.setUpAndSendResponse(res, object, HTTPResponseCode.BAD_REQUEST);
     }
 
     //Pre: Must receive an object that can be converted to standard body media type.
-    //Pre: body must be in standard body media type format.
+    //Post: Sets the object as response body and sends the response with status code 401.
     protected unauthorizedResponse<T>(res: Response, object: T): void {
         this.setUpAndSendResponse(res, object, HTTPResponseCode.UNAUTHORIZED);
     }
 
     //Pre: Must receive an object that can be converted to standard body media type.
-    //Pre: body must be in standard body media type format.
+    //Post: Sets the object as response body and sends the response with status code 404.
     protected notFoundResponse<T>(res: Response, object: T): void {
         this.setUpAndSendResponse(res, object, HTTPResponseCode.NOT_FOUND);
     }
 
     //Pre: Must receive an object that can be converted to standard body media type.
-    //Pre: body must be in standard body media type format.
+    //Post: Sets the object as response body and sends the response with status code 500.
     protected internalServerErrorResponse<T>(res: Response, object: T): void {
         this.setUpAndSendResponse(res, object, HTTPResponseCode.INTERNAL_SERVER_ERROR);
     }
 
+    //Pre: Must receive an object that can be converted to standard body media type.
+    //Post: Sets the object as response body and sends the response with the given status code.
     private setUpAndSendResponse(res: Response, object: any, statusCode: number): void {
         this.setResponseBody(res, object);
         this.sendResponseWithStatusCode(res, statusCode);
