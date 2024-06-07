890dc1ade8139c8228c23c05037801292ed2aab6
Bruno Quiroga <bquiroga@fi.uba.ar>
2024-06-07 18:57:54 -0300

    integracion fotos de imgur

--- client/app/(tabs)/matching.tsx
+++ client/app/(tabs)/matching.tsx
@@ -1,72 +1,93 @@
 import { StyleSheet } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Swiper from 'react-native-deck-swiper';
 import { ProfileCard, Profile, ProfileCardProps } from '@/components/ProfileCard';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
-const profiles: Profile[] = [
-  {
-    name: 'John Doe',
-    age: 30,
-    city: 'New York',
-    description: 'Loves hiking and outdoor adventures.',
-    image: 'https://example.com/john.jpg',
-    knownLanguages: ['English', 'Spanish'],
-    wantToKnowLanguages: ['French'],
-  },
-  {
-    name: 'Jane Smith',
-    age: 25,
-    city: 'San Francisco',
-    description: 'Enjoys reading and quiet evenings at home.',
-    image: 'https://example.com/jane.jpg',
-    knownLanguages: ['English', 'French'],
-    wantToKnowLanguages: ['Spanish'],
-  },
-  {
-    name: 'Alice Johnson',
-    age: 35,
-    city: 'Los Angeles',
-    description: 'Passionate about food and cooking.',
-    image: 'https://example.com/alice.jpg',
-    knownLanguages: ['English', 'Italian'],
-    wantToKnowLanguages: ['Japanese'],
-  },
-  {
-    name: 'Bob Brown',
-    age: 28,
-    city: 'Chicago',
-    description: 'Loves playing sports and staying active.',
-    image: 'https://example.com/bob.jpg',
-    knownLanguages: ['English', 'German'],
-    wantToKnowLanguages: ['Chinese'],
-  }
-];
-
 export default function MatchngScreen() {
-  const [profileIndex, setProfileIndex] = useState(0);
+
+  const profiles: Profile[] = [
+    {
+      name: 'John Doe',
+      age: 30,
+      city: 'New York',
+      description: 'Loves hiking and outdoor adventures.',
+      image: 'https://i.imgur.com/IM82bWA.jpeg',
+      knownLanguages: ['English', 'Spanish'],
+      wantToKnowLanguages: ['French'],
+    },
+    {
+      name: 'Jane Smith',
+      age: 25,
+      city: 'San Francisco',
+      description: 'Enjoys reading and quiet evenings at home.',
+      image: 'https://i.imgur.com/tfUxQbf.jpeg',
+      knownLanguages: ['English', 'French'],
+      wantToKnowLanguages: ['Spanish'],
+    },
+    {
+      name: 'Alice Johnson',
+      age: 35,
+      city: 'Los Angeles',
+      description: 'Passionate about food and cooking.',
+      image: 'https://i.imgur.com/eiwJg1P.jpeg',
+      knownLanguages: ['English', 'Italian'],
+      wantToKnowLanguages: ['Japanese'],
+    },
+    {
+      name: 'Bob Brown',
+      age: 28,
+      city: 'Chicago',
+      description: 'Loves playing sports and staying active.',
+      image: 'https://i.imgur.com/38g0nji.jpeg',
+      knownLanguages: ['English', 'German'],
+      wantToKnowLanguages: ['Chinese'],
+    }
+  ];
+  
+  const getNewProfile = (): Profile => {
+    return profiles[Math.floor(Math.random() * profiles.length)];
+    //LLAMADA A API
+  };
+
+
+  const [currentProfile, setCurrentProfile] = useState<Profile | null>(null);
+
+  useEffect(() => {
+    setCurrentProfile(getNewProfile());
+  }, []);
+
+  const handleSwiped = () => {
+    setCurrentProfile(getNewProfile());
+  };
+
+  const handleSwipedLeft = () => {
+    console.log('Swiped left');
+    handleSwiped();
+  };
+
+  const handleSwipedRight = () => {
+    console.log('Swiped right');
+    handleSwiped();
+  };
 
   return (
     <View style={styles.container}>
-      <Swiper
-        cards={profiles}
+      {currentProfile && (
+        <Swiper
+        cards={[currentProfile]}
         renderCard={(profile: Profile) => <ProfileCard profile={profile} />}
-        onSwiped={(cardIndex: number) => {
-          console.log('Card swiped at index:', cardIndex);
-          setProfileIndex(cardIndex + 1);
-        }}
-        onSwipedAll={() => {
-          console.log('All cards swiped');
-        }}
-        cardIndex={profileIndex}
+        onSwipedLeft={handleSwipedLeft}
+        onSwipedRight={handleSwipedRight}
         backgroundColor={'#f0f0f0'}
-        stackSize={3}
+        stackSize={1}
       />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
