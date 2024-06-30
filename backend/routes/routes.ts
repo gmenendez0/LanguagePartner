96c57daa9a3dbda0bb9f28004448c4a2064d87e0
Guido Menendez <guidomenendez0@gmail.com>
2024-06-30 01:42:34 -0300

    Reordenamiento de directorios y borrado de archivos viejos

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -1,36 +1,16 @@
 import { Router } from 'express';
-/*import * as userController from '../controllers/userController';
-import * as matchingController from '../controllers/matchingController';
-import * as imageController from '../controllers/imageController';*/
 import languageRouter from "./languageRoutes";
 import sessionRouter from "./sessionRoutes";
 import userRouter from "./userRoutes";
 import chatRouter from './chatRoutes';
-import imageRouter from './imageRoutes';
 import matchingRouter from './matchingRoutes';
 
 const router = Router();
-
-/*router.post('/add-known-language', userController.addKnownLanguage);
-router.post('/add-wanted-language', userController.addWantedLanguage);
-router.delete('/remove-known-language', userController.removeKnownLanguage);
-router.delete('/remove-wanted-language', userController.removeWantedLanguage);
-router.get('/my-languages', userController.myLanguages);
-
-router.get('/matchable-user', matchingController.getMatchableUser);
-router.post('/approve-user', matchingController.approveUser);
-router.post('/reject-user', matchingController.rejectUser);
-router.get('/relationships', matchingController.getRelationships);
-
-router.post('/upload-profile-picture', imageController.uploadMiddleware,  imageController.uploadProfilePicture);
-router.delete('/delete-profile-picture', imageController.deleteProfilePicture);
-router.get('/profile-picture', imageController.getProfilePicture);*/
 
 router.use('/v1/session', sessionRouter);
 router.use('/v1/language', languageRouter);
 router.use('/v1/user', userRouter);
 router.use('/v1/chat', chatRouter);
-router.use('/v1/image', imageRouter);
 router.use('/v1/matching', matchingRouter);
 
 export default router;
