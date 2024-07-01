fee6dedd0b76ebfb9aa963843ff0ee5fe143d013
Bruno Quiroga <bquiroga@fi.uba.ar>
2024-07-01 16:43:29 -0300

    hotfix image default

--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
 const PASSPORT_AUTH_STRATEGY = process.env.PASSPORT_AUTH_STRATEGY || 'jwt';
 const JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME || '1h';
 const HASH_SALT_ROUNDS = process.env.HASH_SALT_ROUNDS || 10;
-const DEFAULT_USER_PIC_HASH = process.env.DEFAULT_USER_PIC_HASH || 'default_user_pic_hash';
+const DEFAULT_USER_PIC_HASH = process.env.DEFAULT_USER_PIC_HASH || 'JkzGSGZ';
 const IMGUR_TOKEN = process.env.IMGUR_TOKEN || 'faf88da0b48658a09e9fce28e6659a3b7c2310ff';
 
 export {
